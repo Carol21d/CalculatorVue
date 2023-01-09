262049b9e1c1ee7ca2ff8a7c10014f1c0439e8fd
diegofred10 <diegoa.gsm@gmail.com>
2023-01-09 06:49:59 +0100

    casi terminado conversor

--- Calculator-Vue/src/components/TheWelcome.vue
+++ Calculator-Vue/src/components/TheWelcome.vue
@@ -1,162 +1,147 @@
+
 <script setup>
-// const monedaEl_one = document.getElementById('moneda-uno');
-// const monedaEl_two = document.getElementById('moneda-dos');
-// const cantidadEl_one = document.getElementById('cantidad-uno');
-// const cantidadEl_two = document.getElementById('cantidad-dos');
-// const cambioEl = document.getElementById('cambio');
-// const tazaEl = document.getElementById('taza');
+function calculate() {
+    let valor = document.getElementById("valor").value;
+    let resultado;
+    let dolar = 1.06
+    let yenchino = 140.51
+    let lempira = 26.26
 
-//  function calculate(){
-//     const moneda_one = monedaEl_one.value;
-//     const moneda_two = monedaEl_two.value;
+    if (valor=="usd") {
+        resultado = valor * dolar
+        alert("El cambio a Dolares es: $" + resultado);
+    }
+    else if (valor=="yen") {
+        resultado = valor * yenchino
+        alert("El cambio a Yenes es : Y" + resultado);
 
-//    fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
-//    .then(res => res.json() )
-//    .then(data => {
-//        const taza = data.rates[moneda_two];
+    }
+    else if (valor=="hon") {
+        resultado = valor * lempira
+        alert("El cambio a Lempiras es : H" + resultado);
+    }
+    else {
+        alert("Seleccione una divisa e introduzca un valor para obtener el cambio")
 
-//        cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+    }
+}
 
-//        cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
 
-//     } );
-
-// }
-
-// monedaEl_one.addEventListener('change', calculate);
-// cantidadEl_one.addEventListener('input', calculate);
-// monedaEl_two.addEventListener('change', calculate);
-// cantidadEl_two.addEventListener('input', calculate);
-
-// taza.addEventListener('click', () =>{
-//     const temp = monedaEl_one.value;
-//     monedaEl_one.value = monedaEl_two.value;
-//     monedaEl_two.value = temp;
-//     calculate();
-// })
-//     calculate();
 </script>
 
+
 <template>
-  <h1>Conversor de moneda</h1>
-  <p>Escoje la moneda y la cantidad para realizar la conversion</p>
 
-  <div class="container">
-    <div class="moneda">
-      <select id="moneda-uno">
-        <option value="">usd</option>
-        <option value="">eur</option>
-        <option value="">yen</option>
-        <option value="">hon</option>
-      </select>
-      <input type="number" id="cantidad-uno" placeholder="0" value="1" />
-    </div>
+    <h1>Conversor de moneda</h1>
+    <p>Escoje la moneda y la cantidad para realizar la conversion</p>
 
-    <div class="taza-cambio-container">
-      <button class="btn" id="taza">cambio</button>
-      <div class="cambio" id="cambio"></div>
-    </div>
     
-    <div class="moneda">
-        <select id="moneda-dos">
-          <option value="">usd</option>
-          <option value="">eur</option>
-          <option value="">yen</option>
-          <option value="">hon</option>
-        </select>
-        <input type="number" id="cantidad-dos" placeholder="0" />
-    </div>
-  </div>
+        <div class="moneda">
+            <select v-model="selected">
+                <option value="usd">usd</option>
+                <option value="yen">yen</option>
+                <option value="hon">hon</option>
+            </select>
+            
+ 
+<span> {{ selected }}</span>
+
+
+            <input type="number" placeholder="0" v-model="valor">
+
+        </div>
+
+            <button class="btn" v-on:click="calculate" >
+                cambio
+            </button>
 
 
 </template>
 
-<style lang="css">
+<style>
 :root {
-  --primary-color: #f5ce52;
+    --primary-color: #dbb63a;
 }
 
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 body {
-  font-family: Arial, Helvetica, sans-serif;
-  background-color: #f4f4f4;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  margin: 0;
-  padding: 20px;
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #f4f4f4;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    padding: 20px;
 }
 
 h1 {
-  color: var(--primary-color);
+    color: var(--primary-color);
 }
 
 p {
-  text-align: center;
+    text-align: center;
 }
 
 .btn {
-  color: white;
-  background: var(--primary-color);
-  cursor: pointer;
-  border-radius: 5px;
-  font-size: 12px;
-  padding: 5px 12px;
+    color: rgb(12, 1, 1);
+    background: var(--primary-color);
+    cursor: pointer;
+    border-radius: 5px;
+    font-size: 12px;
+    padding: 5px 12px;
 }
 
-.conversor-imagen {
-  width: 150px;
-}
 
 .moneda {
-  padding: 40px 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+    padding: 40px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
 
 .moneda select {
-  padding: 10px 20px 10px 10px;
-  border: 1px solid #dedede;
-  font-size: 16px;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  appearance: none;
+    padding: 10px 20px 10px 10px;
+    border: 1px solid #dedede;
+    font-size: 16px;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
 }
 
 .moneda input {
-  border: 0;
-  font-size: 30px;
-  text-align: right;
+    border: 0;
+    font-size: 30px;
+    text-align: right;
 }
 
-.taza-cambio-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+.tasa-cambio-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
 
 .cambio {
-  color: var(--primary-color);
-  font-size: 14px;
-  padding: 0 10px;
-  font-weight: 600;
+    color: var(--primary-color);
+    font-size: 14px;
+    padding: 0 10px;
+    font-weight: 600;
 }
 
 select:focus,
 input:focus,
 button:focus {
-  outline: 0;
+    outline: 0;
 }
 
-@media (max-width: 600px) {
-  .moneda input {
-    width: 200px;
-  }
+@media (max-width:600px) {
+    .moneda input {
+        width: 200px;
+    }
+
 }
 </style>
