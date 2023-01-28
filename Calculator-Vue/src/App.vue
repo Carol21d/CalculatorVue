42ad1874582e650c2aa57f2cb7a5c52e87694d57
diegofred10 <diegoa.gsm@gmail.com>
2023-01-28 18:48:22 +0100

    commit

--- Calculator-Vue/src/App.vue
+++ Calculator-Vue/src/App.vue
@@ -1,14 +1,29 @@
-<script setup>
-import TheWelcome from './components/TheWelcome.vue'
-
-</script>
-
 <template>
+    <div class="fondo">
+      <Navbar />
+      <Convertidor />
+    </div>
+  </template>
   
-    <TheWelcome />
+  <script>
+  import Convertidor from "./components/Convertidor";
+  import Navbar from "./components/Navbar";
+  export default {
+    name: "App",
+    components: {
+      Convertidor,
+      Navbar,
+    },
+  };
+  </script>
   
-</template>
-
-<style scoped>
-
-</style>
+  <style>
+  .fondo {
+    background-image: url("./assets/fondo.jpg");
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  </style>
+  
